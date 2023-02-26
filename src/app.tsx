c0bc9d945eefc91912ad0db9afcfb5f79e2f1b18
harrytran998 <nhattq.coding@gmail.com>
2023-02-26 23:16:54 +0700

    1 file modified

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from 'preact/hooks';
+import preactLogo from './assets/preact.svg';
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <p class="text-red">Hello</p>
+      <div class="text-white p-2 bg-blue-400 rounded-full uppercase hover:(text-red bg-purple-300)">
+        <h5 class="text-2xl">Hello</h5>
+      </div>
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
 }
