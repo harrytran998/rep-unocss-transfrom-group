94c78c4bfb951114d34a95583c75fb38aa0692b7
harrytran998 <nhattq.coding@gmail.com>
2023-02-26 23:13:02 +0700

    5 files modified, 1 added

--- src/app.tsx
+++ src/app.tsx
@@ -7,23 +7,7 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <p class="text-red">Hello</p>
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
-import { App } from './app'
-import './index.css'
+import { render } from 'preact';
+import { App } from './app';
+import './index.css';
+import 'uno.css';
 
-render(<App />, document.getElementById('app') as HTMLElement)
+render(<App />, document.getElementById('app') as HTMLElement);
